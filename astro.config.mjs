e24f37c4ccfdf0afe7a70ac79f4a307b5d43c03c
iqbalrachman <139888864+iqbalrachman@users.noreply.github.com>
2026-07-23 13:16:44 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'static',
-  build: {
-    format: 'directory'
-  }
+  build: { format: 'directory' },
+  integrations: [tailwind()]
 });
