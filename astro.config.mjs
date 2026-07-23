555f8b1c934ad62c95e017a9663e4e965133d955
iqbalrachman <139888864+iqbalrachman@users.noreply.github.com>
2026-07-23 13:28:43 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  // INI PENYELAMATNYA: Ngarahin CSS ke folder repo yang benar
+  site: 'https://iqbalrachman.github.io',
+  base: '/gemini-version_pkp2-extension', 
+  
   output: 'static',
   build: { format: 'directory' },
   integrations: [tailwind()]
